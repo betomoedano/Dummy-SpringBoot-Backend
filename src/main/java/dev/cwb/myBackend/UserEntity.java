8d3bec10b42aed041e17e7ef243100137b1bba4a
betomoedano <betomoedano@outlook.com>
2023-12-02 15:38:20 -0600

    hello

--- src/main/java/dev/cwb/myBackend/UserEntity.java
+++ src/main/java/dev/cwb/myBackend/UserEntity.java
@@ -1,80 +1,104 @@
 package dev.cwb.myBackend;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "users") // Specify the table name if it's different from the class name
+@Table(name = "Users") // Ensure this matches your SQL table name
 public class UserEntity {
 
     @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private UUID id;
 
-    private String username;
+    private String firstName;
+
+    private String lastName;
+
     private String email;
-    private LocalDateTime created_at;
+
+    private String picture;
+    
+    private Boolean preferredCustomer;
+
+    @Column(name = "createdAt")
+    private LocalDateTime createdAt;
 
     // No-arg constructor
     public UserEntity() {
     }
 
-    // All-args constructor
-    public UserEntity(Long id, String username, String email, LocalDateTime created_at) {
-        this.id = id;
-        this.username = username;
+    // Constructor without id and createdAt, as these are auto-generated
+    public UserEntity(String firstName, String lastName, String email, String picture, Boolean preferredCustomer) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
-        this.created_at = created_at;
+        this.picture = picture;
+        this.preferredCustomer = preferredCustomer;
     }
 
     @PrePersist
     protected void onCreate() {
-      created_at = LocalDateTime.now();
+        createdAt = LocalDateTime.now();
     }
 
-    // Getters
-    public Long getId() {
+    public UUID getId() {
         return id;
     }
 
-    public String getUsername() {
-        return username;
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public LocalDateTime getCreatedAt() {
-        return created_at;
-    }
-
-    // Setters
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public void setCreatedAt(LocalDateTime created_at) {
-        this.created_at = created_at;
+    public String getPicture() {
+        return picture;
     }
 
-    // toString method
-    @Override
-    public String toString() {
-        return "UserEntity{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", email='" + email + '\'' +
-                ", created_at='" + created_at + '\'' +
-                '}';
+    public void setPicture(String picture) {
+        this.picture = picture;
     }
+
+    public Boolean getPreferredCustomer() {
+        return preferredCustomer;
+    }
+
+    public void setPreferredCustomer(Boolean preferredCustomer) {
+        this.preferredCustomer = preferredCustomer;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+
 }
